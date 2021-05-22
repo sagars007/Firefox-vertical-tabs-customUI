bb66f1b2966db75efc6583e9ed97c67312fdb8b1
Sagar S <56472130+sagars007@users.noreply.github.com>
2021-05-22 21:03:10 +0400

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,23 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.urlbar.update1", false);
+
+user_pref("ui.systemUsesDarkTheme", 1);
+
+user_pref("general.smoothScroll.msdPhysics.enabled", true);
+user_pref("general.smoothScroll.msdPhysics.continuousMotionMaxDeltaMS", 250);
+user_pref("general.smoothScroll.msdPhysics.motionBeginSpringConstant", 400);
+user_pref("general.smoothScroll.msdPhysics.regularSpringConstant", 400);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaMS", 120);
+user_pref("general.smoothScroll.msdPhysics.slowdownMinDeltaRatio", 0.4);
+user_pref("general.smoothScroll.msdPhysics.slowdownSpringConstant", 5000);
+user_pref("mousewheel.min_line_scroll_amount", 22);
+user_pref("toolkit.scrollbox.horizontalScrollDistance", 4);
+user_pref("toolkit.scrollbox.verticalScrollDistance", 5);
+user_pref("apz.frame_delay.enabled", false);
+
+//Full screen fade faster:
+user_pref("full-screen-api.transition-duration.enter", 50 50);
+user_pref("full-screen-api.transition-duration.leave", 50 50);
+
+//always show firefox logo in default newtab page:
+user_pref("browser.newtabpage.activity-stream.logowordmark.alwaysVisible", true);
